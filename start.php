5735f569a6db893a61c80d62c3537436ab1e7440
Pader <ypnow@163.com>
2020-10-06 01:24:26 +0800

    增加 runtime 目录存储运行中文件

--- start.php
+++ start.php
@@ -6,9 +6,16 @@ use Workerman\Worker;
 require __DIR__.'/vendor/autoload.php';
 
 define('BASE_DIR', __DIR__);
+define('RUNTIME_DIR', __DIR__.'/runtime');
+
+Worker::$logFile = RUNTIME_DIR.'/workerman.log';
+Worker::$pidFile = RUNTIME_DIR.'/workerman-amphp.pid';
+
+if (!is_dir(RUNTIME_DIR)) {
+    mkdir(RUNTIME_DIR, 0775);
+}
 
 $application = new Application();
 $application->addComponent(\Framework\Process\Component::class);
 
-Worker::$pidFile = __DIR__.'/workerman-amphp.pid';
 Worker::runAll();
--- test.php
+++ test.php
@@ -1,12 +1,14 @@
 <?php
-// using Loop::defer()
 
 use Workerman\Worker;
 
 require __DIR__.'/vendor/autoload.php';
 
+Worker::$pidFile = __DIR__.'/runtime/test.pid';
+Worker::$logFile = __DIR__.'/runtime/test.log';
+
 $worker = new Worker('http://0.0.0.0:2346');
-$worker->count = 2;
+$worker->count = 10;
 $worker->onMessage = function($connection, $request) {
     sleep(5);
     $connection->send('Block sleep 5 seconds.');

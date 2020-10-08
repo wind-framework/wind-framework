7d47a22aec96d7c5f1b0ec11b9aa550127d94ba0
杨培 <ypnow@163.com>
2020-10-09 00:24:10 +0800

    优化 Twig View 引擎，无需注册组件，不使用时不实例化 Twig。

--- config/component.php
+++ config/component.php
@@ -3,6 +3,5 @@
 //要启用的组件列表
 return [
     \Framework\Process\Component::class,
-    \Framework\Collector\Component::class,
-    \Framework\View\Component::class
+    \Framework\Collector\Component::class
 ];
--- framework/View/Twig.php
+++ framework/View/Twig.php
@@ -2,23 +2,35 @@
 
 namespace Framework\View;
 
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
 class Twig
 {
 
     private static $twig;
 
-    public function __construct($viewDir, $cacheDir)
-    {
-        $loader = new \Twig\Loader\FilesystemLoader($viewDir);
-        self::$twig = new \Twig\Environment($loader, [
-            'cache' => $cacheDir,
-            'auto_reload' => true
-        ]);
+	/**
+	 * @return Environment
+	 */
+    protected static function twig() {
+    	if (self::$twig instanceof Environment) {
+    		return self::$twig;
+	    }
+
+	    $viewDir = BASE_DIR.'/view';
+	    $cacheDir = RUNTIME_DIR.'/view';
+
+	    $loader = new FilesystemLoader($viewDir);
+	    return self::$twig = new Environment($loader, [
+		    'cache' => $cacheDir,
+		    'auto_reload' => true
+	    ]);
     }
 
     public static function render($name, array $context = []): string
     {
-        return self::$twig->render($name, $context);
+        return self::twig()->render($name, $context);
     }
 
 }

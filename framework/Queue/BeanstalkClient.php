de741a4f3e3eb0c8e99d6d821b0c6900de27505d
Pader <ypnow@163.com>
2020-10-30 18:53:11 +0800

    Beanstalkd 连接的中断和重连

--- framework/Queue/BeanstalkClient.php
+++ framework/Queue/BeanstalkClient.php
@@ -2,10 +2,14 @@
 
 namespace Framework\Queue;
 
+use Exception;
+use Amp\Success;
 use Amp\Deferred;
-use Framework\Utils\ArrayUtil;
 use RuntimeException;
 use function Amp\call;
+
+use function Amp\asyncCall;
+use Framework\Utils\ArrayUtil;
 
 use Workerman\Connection\AsyncTcpConnection;
 
@@ -15,21 +19,29 @@ class BeanstalkClient
 	const DEFAULT_PRI = 60;
 	const DEFAULT_TTR = 30;
 
-	protected $config;
-	protected $connection;
-    protected $lastError = null;
+	private $config;
+	private $connection;
     private $connected = false;
 
     private $tubeUsed = 'default';
     private $watchTubes = ['default'];
+    private $reserveCmd = '';
 
     private $onConnectCallback = null;
     private $onErrorCallback = null;
+    private $onCloseCallback = null;
+
+    /**
+     * Reading Deferred
+     *
+     * @var Deferred
+     */
+    public $reading = null;
 
 	public $debug = false;
 
 	/**
-	 * Swbeanstalk constructor.
+	 * BeanstalkClient constructor.
 	 *
 	 * @param string $host
 	 * @param int $port
@@ -38,18 +50,21 @@ class BeanstalkClient
 	 */
 	public function __construct($host='127.0.0.1', $port=11300, $connectTimeout=1, $timeout=-1)
 	{
-		$this->config = compact('host', 'port');
-        $this->connection = new AsyncTcpConnection("tcp://$host:$port");
+        $this->config = compact('host', 'port');
+        $this->connection = new AsyncTcpConnection("tcp://{$this->config['host']}:{$this->config['port']}");
+	}
+
+	public function connect()
+	{
+		if ($this->connected) {
+			return new Success();
+        }
 
         $this->connection->onConnect = function(...$args) {
             $this->connected = true;
-            if (is_callable($this->onConnectCallback)) {
-                call_user_func_array($this->onConnectCallback, $args);
-                $this->onConnectCallback = null;
-            }
 
             //重连后恢复相应 tube 的监听和使用
-            call(function() {
+            asyncCall(function() use ($args) {
                 if ($this->tubeUsed != 'default') {
                     yield $this->useTube($this->tubeUsed);
                 }
@@ -67,27 +82,41 @@ class BeanstalkClient
                 if (!$watchDefault) {
                     yield $this->ignore('default');
                 }
+
+                if (is_callable($this->onConnectCallback)) {
+                    call_user_func_array($this->onConnectCallback, $args);
+                    $this->onConnectCallback = null;
+                }
+
+                if ($this->reading) {
+                    $this->reading->fail(new RuntimeException('Connection closed.'));
+                    $this->reading = null;
+                }
             });
         };
         
         $this->connection->onError = function($connection, $code, $message) {
-            $this->connected = false;
+            echo "Connection Error: [$code] $message\n";
             if (is_callable($this->onErrorCallback)) {
-                call_user_func($this->onErrorCallback, $connection, $connection, $message);
+                call_user_func($this->onErrorCallback, $connection, $code, $message);
                 $this->onErrorCallback = null;
             }
         };
 
-        $this->connection->onClose = function($connection) {
+        $this->connection->onClose = function() {
+            echo "Detect connection closed\n";
             $this->connected = false;
-        };
-	}
 
-	public function connect()
-	{
-		if ($this->connected) {
-			$this->connection->close();
-        }
+            if ($this->reading) {
+                $this->reading->fail(new RuntimeException('Connection closed.'));
+                $this->reading = null;
+            }
+
+            if (is_callable($this->onCloseCallback)) {
+                call_user_func($this->onCloseCallback);
+                $this->onCloseCallback = null;
+            }
+        };
 
         $defer = new Deferred();
         
@@ -102,20 +131,41 @@ class BeanstalkClient
         $this->connection->connect();
 
         return $defer->promise();
-	}
+    }
+    
+    /**
+     * 关闭连接
+     *
+     * @return \Amp\Promise
+     */
+    public function close()
+    {
+        if (!$this->connected) {
+            return new Success();
+        }
+
+        $defer = new Deferred();
+
+        $this->onCloseCallback = function() use ($defer) {
+            $defer->resolve();
+        };
+
+        $this->connection->destroy();
+
+        return $defer->promise();
+    }
 
 	public function put($data, $pri=self::DEFAULT_PRI, $delay=0, $ttr=self::DEFAULT_TTR)
 	{
-        $this->send(sprintf("put %d %d %d %d\r\n%s", $pri, $delay, $ttr, strlen($data), $data));
+        $cmd = sprintf("put %d %d %d %d\r\n%s", $pri, $delay, $ttr, strlen($data), $data);
 
-        return call(function() {
-            $res = yield $this->recv();
+        return call(function() use ($cmd) {
+            $res = yield $this->send($cmd);
     
             if ($res['status'] == 'INSERTED') {
                 return $res['meta'][0];
             } else {
-                $this->setError($res['status']);
-                return false;
+                throw new Exception($res['status']);
             }
         });
 	}
@@ -123,28 +173,24 @@ class BeanstalkClient
 	public function useTube($tube)
 	{
         return call(function() use ($tube) {
-            $this->send(sprintf('use %s', $tube));
-            $ret = yield $this->recv();
+            $ret = yield $this->send(sprintf('use %s', $tube));
             if ($ret['status'] == 'USING' && $ret['meta'][0] == $tube) {
                 $this->tubeUsed = $tube;
                 return true;
             } else {
-                $this->setError($ret['status'], "Use tube $tube failed.");
-                return false;
+                throw new Exception($ret['status'].": Use tube $tube failed.");
             }
         });
 	}
 
 	public function reserve($timeout=null)
 	{
-		if (isset($timeout)) {
-			$this->send(sprintf('reserve-with-timeout %d', $timeout));
-		} else {
-			$this->send('reserve');
-		}
+        $cmd = isset($timeout) ? sprintf('reserve-with-timeout %d', $timeout) : 'reserve';
+        $this->reserveCmd = $cmd;
         
-        return call(function() {
-            $res = yield $this->recv(true, 1);
+        return call(function() use ($cmd) {
+            $res = yield $this->send($cmd, null, true, 1);
+            $this->reserveCmd = '';
 
             if ($res['status'] == 'RESERVED') {
                 list($id, $bytes) = $res['meta'];
@@ -154,37 +200,35 @@ class BeanstalkClient
                     'bytes' => $bytes
                 ];
             } else {
-                $this->setError($res['status']);
-                return false;
+                throw new Exception($res['status']);
             }
         });
 	}
 
 	public function delete($id)
 	{
-		return $this->sendv(sprintf('delete %d', $id), 'DELETED');
+		return $this->send(sprintf('delete %d', $id), 'DELETED');
 	}
 
 	public function release($id, $pri=self::DEFAULT_PRI, $delay=0)
 	{
-		return $this->sendv(sprintf('release %d %d %d', $id, $pri, $delay), 'RELEASED');
+		return $this->send(sprintf('release %d %d %d', $id, $pri, $delay), 'RELEASED');
 	}
 
 	public function bury($id)
 	{
-		return $this->sendv(sprintf('bury %d', $id), 'BURIED');
+		return $this->send(sprintf('bury %d', $id), 'BURIED');
 	}
 
 	public function touch($id)
 	{
-		return $this->sendv(sprintf('touch %d', $id), 'TOUCHED');
+		return $this->send(sprintf('touch %d', $id), 'TOUCHED');
 	}
 
 	public function watch($tube)
 	{
         return call(function() use ($tube) {
-            $this->send(sprintf('watch %s', $tube));
-            $res = yield $this->recv();
+            $res = yield $this->send(sprintf('watch %s', $tube));
 
             if ($res['status'] == 'WATCHING') {
                 if (!in_array($tube, $this->watchTubes)) {
@@ -192,8 +236,7 @@ class BeanstalkClient
                 }
                 return $res['meta'][0];
             } else {
-                $this->setError($res['status']);
-                return false;
+                throw new Exception($res['status']);
             }
         });
 	}
@@ -201,47 +244,41 @@ class BeanstalkClient
 	public function ignore($tube)
 	{
         return call(function() use ($tube) {
-            $this->send(sprintf('ignore %s', $tube));
-            $res = yield $this->recv();
+            $res = yield $this->send(sprintf('ignore %s', $tube));
 
             if ($res['status'] == 'WATCHING') {
                 ArrayUtil::removeElement($this->watchTubes, $tube);
                 return $res['meta'][0];
             } else {
-                $this->setError($res['status']);
-                return false;
+                throw new Exception($res['status']);
             }
         });
 	}
 
 	public function peek($id)
 	{
-		$this->send(sprintf('peek %d', $id));
-		return $this->peekRead();
+		return $this->peekRead(sprintf('peek %d', $id));
 	}
 
 	public function peekReady()
 	{
-		$this->send('peek-ready');
-		return $this->peekRead();
+		return $this->peekRead('peek-ready');
 	}
 
 	public function peekDelayed()
 	{
-		$this->send('peek-delayed');
-		return $this->peekRead();
+		return $this->peekRead('peek-delayed');
 	}
 
 	public function peekBuried()
 	{
-		$this->send('peek-buried');
-		return $this->peekRead();
+		return $this->peekRead('peek-buried');
 	}
 
-	protected function peekRead()
+	protected function peekRead($cmd)
 	{
-        return call(function() {
-            $res = yield $this->recv(true, 1);
+        return call(function() use ($cmd) {
+            $res = yield $this->send($cmd, null, true, 1);
 
             if ($res['status'] == 'FOUND') {
                 list($id, $bytes) = $res['meta'];
@@ -251,82 +288,70 @@ class BeanstalkClient
                     'bytes' => $bytes
                 ];
             } else {
-                $this->setError($res['status']);
-                return false;
+                throw new Exception($res['status']);
             }
         });
 	}
 
 	public function kick($bound)
 	{
-        $this->send(sprintf('kick %d', $bound));
-        
-        return call(function() {
-            $res = yield $this->recv();
+        return call(function() use ($bound) {
+            $res = yield $this->send(sprintf('kick %d', $bound));
 
             if ($res['status'] == 'KICKED') {
                 return $res['meta'][0];
             } else {
-                $this->setError($res['status']);
-                return false;
+                throw new Exception($res['status']);
             }
         });
 	}
 
 	public function kickJob($id)
 	{
-		return $this->sendv(sprintf('kick-job %d', $id), 'KICKED');
+		return $this->send(sprintf('kick-job %d', $id), 'KICKED');
 	}
 
 	public function statsJob($id)
 	{
-		$this->send(sprintf('stats-job %d', $id));
-		return $this->statsRead();
+		return $this->statsRead(sprintf('stats-job %d', $id));
 	}
 
 	public function statsTube($tube)
 	{
-		$this->send(sprintf('stats-tube %s', $tube));
-		return $this->statsRead();
+		return $this->statsRead(sprintf('stats-tube %s', $tube));
 	}
 
 	public function stats()
 	{
-		$this->send('stats');
-		return $this->statsRead();
+		return $this->statsRead('stats');
 	}
 
 	public function listTubes()
 	{
-		$this->send('list-tubes');
-		return $this->statsRead();
+		return $this->statsRead('list-tubes');
 	}
 
 	public function listTubeUsed()
 	{
-        $this->send('list-tube-used');
-        
         return call(function() {
-            $res = yield $this->recv();
+            $res = yield $this->send('list-tube-used');
             if ($res['status'] == 'USING') {
                 return $res['meta'][0];
             } else {
-                $this->setError($res['status']);
-                return false;
+                throw new Exception($res['status']);
             }
         });
 	}
 
 	public function listTubesWatched()
 	{
-		$this->send('list-tubes-watched');
-		return $this->statsRead();
+		return $this->statsRead('list-tubes-watched');
 	}
 
-	protected function statsRead()
+	protected function statsRead($cmd)
 	{
-        return call(function() {
-            $res = yield $this->recv(true, 0);
+        return call(function() use ($cmd) {
+            $res = yield $this->send($cmd, null, true, 0);
 
             if ($res['status'] == 'OK') {
                 $data = array_slice(explode("\n", $res['body']), 1);
@@ -348,60 +373,30 @@ class BeanstalkClient
                 }
                 return $result;
             } else {
-                $this->setError($res['status']);
-                return false;
+                throw new Exception($res['status']);
             }
         });
 	}
 
 	public function pauseTube($tube, $delay)
 	{
-		return $this->sendv(sprintf('pause-tube %s %d', $tube, $delay), 'PAUSED');
+		return $this->send(sprintf('pause-tube %s %d', $tube, $delay), 'PAUSED');
 	}
 
-	protected function sendv($cmd, $status)
-	{
-        $this->send($cmd);
-        
-        return call(function() use ($status) {
-            $res = yield $this->recv();
-
-            if ($res['status'] != $status) {
-                $this->setError($res['status']);
-                return false;
-            }
-    
-            return true;
-        });
-	}
-
-	protected function send($cmd)
+    /**
+     * 发送命令并接收数据
+     *
+     * @param string $cmd
+     * @param string $status 确认成功匹配状态
+     * @param bool $chunk 是否大数据分块模式
+     * @param int $bytesMetaPos 大数据分块长度数据所在位置，即状态如 "RESERVED" 后的描述，从0开始
+     * @return Promise<Array>
+     */
+	protected function send($cmd, $status=null, $chunk=false, $bytesMetaPos=0)
 	{
 		if (!$this->connected) {
 			throw new \RuntimeException('No connecting found while writing data to socket.');
 		}
-
-		$cmd .= "\r\n";
-
-		if ($this->debug) {
-			$this->wrap($cmd, true);
-		}
-
-		return $this->connection->send($cmd);
-	}
-
-    /**
-     * 接收数据
-     *
-     * @param bool $chunk 是否大数据分块模式
-     * @param int $bytesMetaPos 大数据分块长度数据所在位置，即状态如 "RESERVED" 后的描述，从0开始
-     * @return void
-     */
-	protected function recv($chunk=false, $bytesMetaPos=0)
-	{
-		if (!$this->connected) {
-			throw new \RuntimeException('No connection found while reading data from socket.');
-        }
         
         $defer = new Deferred;
 
@@ -411,7 +406,7 @@ class BeanstalkClient
             'body' => ''
         ];
 
-        $this->connection->onMessage = function($connection, $recv) use ($defer, $chunk, &$data, $bytesMetaPos) {
+        $this->connection->onMessage = function($connection, $recv) use ($defer, $status, $chunk, &$data, $bytesMetaPos) {
             if ($this->debug) {
                 $this->wrap($recv, false);
             }
@@ -428,9 +423,33 @@ class BeanstalkClient
             }
 
             if (!$chunk || strlen($data['body']) == $data['meta'][$bytesMetaPos]) {
-                $defer->resolve($data);
+                $connection->onMessage = null;
+                $this->reading = null;
+                if ($status !== null) {
+                    //消息状态确认
+                    if ($data['status'] != $status) {
+                        $defer->fail(new \Exception($data['status']));
+                    } else {
+                        $defer->resolve();
+                    }
+                } else {
+                    $defer->resolve($data);
+                }
             }
         };
+
+        
+        //发送命令
+        $cmd .= "\r\n";
+        
+        // debug_print_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 20);
+
+		if ($this->debug) {
+			$this->wrap($cmd, true);
+        }
+
+        $this->connection->send($cmd);
+        $this->reading = $defer;
 
         return $defer->promise();
 	}
@@ -441,21 +460,6 @@ class BeanstalkClient
 			$this->send('quit');
 			$this->connection->close();
 		}
-	}
-
-	protected function setError($status, $msg='')
-	{
-		$this->lastError = compact('status', 'msg');
-	}
-
-	public function getError()
-	{
-		if ($this->lastError) {
-			$error = $this->lastError;
-			$this->lastError = null;
-			return $error;
-		}
-		return null;
 	}
 
 	protected function wrap($output, $out)
--- test.php
+++ test.php
@@ -5,6 +5,9 @@ use Framework\Base\Amp;
 
 use function Amp\delay;
 use function Amp\asyncCall;
+use function Amp\asyncCoroutine;
+use function Amp\coroutine;
+
 use Framework\Queue\BeanstalkClient;
 
 require __DIR__.'/vendor/autoload.php';
@@ -13,17 +16,26 @@ Worker::$eventLoopClass = Amp::class;
 Worker::$pidFile = __DIR__.'/runtime/test.pid';
 Worker::$logFile = __DIR__.'/runtime/test.log';
 
+function dumpException(\Throwable $e) {
+    return get_class($e).': ['.$e->getCode().'] '.$e->getMessage()."\n";
+}
+
+define('AMP_DEBUG', true);
+
 $worker = new Worker();
 $worker->reusePort = false;
 $worker->count = 1;
 $worker->onWorkerStart = function() {
+    /*
     asyncCall(function() {
         $client = new BeanstalkClient('192.168.4.2');
         $client->debug = true;
         yield $client->connect();
         echo "Producer connect success.\n";
 
-        for ($i=0; $i<1000; $i++) {
+        yield $client->useTube('test');
+
+        for ($i=0; $i<2; $i++) {
             echo "--producer ";
             $id = yield $client->put("Hello World");
             echo $id."--\n";
@@ -31,17 +43,61 @@ $worker->onWorkerStart = function() {
 
         echo "Put finished.\n";
     });
+    */
 
     asyncCall(function() {
         $client = new BeanstalkClient('192.168.4.2');
         $client->debug = true;
-        yield $client->connect();
-        echo "Consumer connect success.\n";
 
-        while ($data = yield $client->reserve()) {
-            print_r($data);
-            yield $client->delete($data['id']);
-            yield delay(10);
+        delay(2000)->onResolve(function() use ($client) {
+            asyncCall(function() use ($client) {
+                echo "Before close connection\n";
+                yield $client->close();
+                echo "After close connection\n";
+    
+                echo "Wait for reconnect\n";
+                yield delay(1500);
+
+                echo "Reconnect\n";
+                yield $client->connect();
+                echo "Reconnect success.\n";
+    
+                // echo "Start watch\n";
+                // yield $client->watch('test');
+    
+                // echo "Start ignore\n";
+                // yield $client->ignore('default');
+        
+                try {
+                    while ($data = yield $client->reserve()) {
+                        print_r($data);
+                        yield $client->delete($data['id']);
+                        yield delay(10);
+                    }
+                } catch (Exception $e) {
+                    echo 'Reserve Error: '.dumpException($e);
+                }
+            });
+        });
+
+        echo "Start connect\n";
+        yield $client->connect();
+        echo "Connect success.\n";
+
+        echo "Start watch\n";
+        yield $client->watch('test');
+
+        echo "Start ignore\n";
+        yield $client->ignore('default');
+
+        try {
+            while ($data = yield $client->reserve()) {
+                print_r($data);
+                yield $client->delete($data['id']);
+                yield delay(10);
+            }
+        } catch (Exception $e) {
+            echo 'Reserve Error: '.dumpException($e);
         }
     });
 };

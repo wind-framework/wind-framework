87c8a7c69d7e4baff1b12627e5d85322405d6bd6
Pader <ypnow@163.com>
2020-12-08 09:45:29 +0800

    调整 Workerman 自带日志文件的位置

--- src/start.php
+++ src/start.php
@@ -8,7 +8,7 @@ require __DIR__.'/vendor/autoload.php';
 define('BASE_DIR', __DIR__);
 define('RUNTIME_DIR', __DIR__.'/runtime');
 
-Worker::$logFile = RUNTIME_DIR.'/workerman.log';
+Worker::$logFile = RUNTIME_DIR.'/log/workerman.log';
 Worker::$pidFile = RUNTIME_DIR.'/workerman-amphp.pid';
 
 if (!is_dir(RUNTIME_DIR)) {

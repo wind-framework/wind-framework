cc57cc81c47232e5e36544aef468801b8d0c86dd
Pader <ypnow@163.com>
2020-12-10 12:04:35 +0800

    修复数据库连接池配置名称错误及调整配置

--- src/config/database.php
+++ src/config/database.php
@@ -12,8 +12,8 @@ return [
         'charset' => env('DB_CHARSET', 'utf8mb4'),
         'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
         'pool' => [
-            'max_connection' => 100,
-            'max_idle_time' => 60
+            'max_connections' => 30,
+            'max_idle_time' => 90
         ]
     ]
 ];

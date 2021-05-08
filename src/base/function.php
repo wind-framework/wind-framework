69b6c52253d70d9e00697a4623ec15e25cfdddc9
Pader <ypnow@163.com>
2021-05-09 02:35:24 +0800

    Whole New Router and router config, and support group, namespace, prefix, middlewares.

--- src/base/function.php
+++ src/base/function.php
@@ -42,12 +42,13 @@ function env($key, $defaultValue=null) {
 
 /**
  * 智能包装 $callable 让其可放心调用
- * 
+ *
  * 内部自动判断目标方法或函数是否动态或静态，如果是动态则通过容器创建该类并支持依赖注入
  *
  * @param string|array $callable 因为可能传入的方法是动态方法，这时不是一个合法的 callable，所以不能限定类型为 callable
  * @param bool $persistent 是否持久化到容器中，是则使用窗口的 get 方法创建单例，否则使用 make 创建临时对象
  * @return callable 返回可任意调用的 callable
+ * @throws CallableException
  */
 function wrapCallable($callable, $persistent=true) {
     if (is_array($callable)) {
@@ -64,13 +65,17 @@ function wrapCallable($callable, $persistent=true) {
         throw new CallableException("Class '$class' not found.");
     }
 
-    if ((new \ReflectionMethod($class, $method))->isStatic()) {
-        return $callable;
-    } else {
-		//动态类型需要先实例化，这得益于依赖注入才能实现
-		//如果该类的构造函数参数不能依赖注入，则不能通过 Task 动态调用
-		$instance = $persistent ? di()->get($class) : di()->make($class);
-		return [$instance, $method];
+    try {
+        if ((new \ReflectionMethod($class, $method))->isStatic()) {
+            return $callable;
+        } else {
+            //动态类型需要先实例化，这得益于依赖注入才能实现
+            //如果该类的构造函数参数不能依赖注入，则不能通过 Task 动态调用
+            $instance = $persistent ? di()->get($class) : di()->make($class);
+            return [$instance, $method];
+        }
+    } catch (ReflectionException $e) {
+        throw new CallableException($e->getMessage());
     }
 }
 

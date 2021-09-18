3d34138d3bcf187c767ec300b102c73793fee16f
Pader <ypnow@163.com>
2021-09-18 21:27:37 +0800

    Add intersectKeys to ArrayUtil.

--- src/utils/ArrayUtil.php
+++ src/utils/ArrayUtil.php
@@ -5,6 +5,11 @@ namespace Wind\Utils;
 class ArrayUtil
 {
 
+    public static function intersectKeys($array, $keys)
+    {
+        return array_intersect_key($array, array_flip($keys));
+    }
+
     public static function removeElement(&$arr, $element, $strict=false)
     {
         $i = array_search($element, $arr, $strict);

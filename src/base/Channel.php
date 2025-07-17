1fbe38f1a8f8fec6b5267f3158731441f70b96f6
Pader <ypnow@163.com>
2025-07-17 14:46:40 +0800

    Use EventLoop::delay to prevent: "PHP Deprecated: Implicit conversion from float to int loses precision"

--- src/base/Channel.php
+++ src/base/Channel.php
@@ -6,7 +6,7 @@ use Amp\DeferredFuture;
 use Amp\Future;
 use Wind\Base\Exception\RequireChannelException;
 use Channel\Client;
-use Workerman\Timer;
+use Revolt\EventLoop;
 
 /**
  * Class Channel
@@ -45,7 +45,7 @@ class Channel
     /**
      * Get connect Future
      *
-     * @return Future<Amp\T>
+     * @return Future
      */
     protected function connect()
     {
@@ -55,12 +55,14 @@ class Channel
 
         $this->connectDefer = new DeferredFuture();
 
-        Client::$onConnect = function()  {
+        /** @psalm-suppress InvalidPropertyAssignmentValue */
+        Client::$onConnect = function() {
             $this->connected = true;
             $this->connectDefer->complete(null);
         };
 
-        Client::$onClose = function()  {
+        /** @psalm-suppress InvalidPropertyAssignmentValue */
+        Client::$onClose = function() {
             //断线后将 Promise 置于 pending 状态等待重连更新
             //已连接状态代表之前的 connectDefer 已经是 resolved 状态，此时需刷新 connectDefer 来让后面的发送等待
             //否则继续延用之前的 pending 状态的 connectDefer
@@ -70,9 +72,7 @@ class Channel
             $this->connected = false;
         };
 
-        Timer::add(0.5, function() {
-            Client::connect($this->config['addr'] ?? '127.0.0.1', $this->config['port'] ?? 2206);
-        }, [], false);
+        EventLoop::delay(0.2, fn() => Client::connect($this->config['addr'] ?? '127.0.0.1', $this->config['port'] ?? 2206));
 
         return $this->connectDefer->getFuture();
     }
